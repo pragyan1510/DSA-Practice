d3a7232e401c385095bbd1a5141b229863deef76
Firstrepo <spragyan574@gmail.com>
2024-03-01 13:03:46 +0530

    a little change in repo

--- hashtables/main.java
+++ hashtables/main.java
@@ -4,6 +4,8 @@ public class main {
     public static void main(String[] args) {
         HashTable ht = new HashTable();
 
+        //change in repo
+
         ht.set("nails",20);
         ht.set("paint",450);
         ht.set("tiles",23);
