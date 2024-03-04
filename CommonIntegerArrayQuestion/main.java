94aa9b9db29f4c08a6bcb95c2111b4b92028ab59
Firstrepo <spragyan574@gmail.com>
2024-03-04 21:48:50 +0530

    using hashtables

--- CommonIntegerArrayQuestion/main.java
+++ CommonIntegerArrayQuestion/main.java
@@ -1,15 +1,31 @@
 package CommonIntegerArrayQuestion;
 
+import hashtables.HashTable;
+
+import java.util.HashMap;
+
 public class main {
     public static boolean itemincommon(int[] array1,int[] array2){
+//        for (int i: array1){
+//            //this for loop will loop through array1 and i is going to be equal to the value at each index;
+//            for (int j: array2){
+//                if (i==j)return true;
+//            }
+//
+//        }
+//        return false;
+
+        //we can also do this with hash tables
+        HashMap<Integer,Boolean> myhashmap = new HashMap<>();
         for (int i: array1){
-            //this for loop will loop through array1 and i is going to be equal to the value at each index;
-            for (int j: array2){
-                if (i==j)return true;
-            }
+            myhashmap.put(i,true);
 
         }
+        for (int j:array2){
+            if (myhashmap.get(j)!=null)return true;
+        }
         return false;
+
 
     }
 
@@ -18,7 +34,7 @@ public class main {
 
     public static void main(String[] args) {
         int[] array1 = {1,2,3};
-        int[] array2 = {4,5,1};
+        int[] array2 = {4,5,9};
 
         System.out.println(itemincommon(array1,array2));
 
